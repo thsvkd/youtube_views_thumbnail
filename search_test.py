5617f0b30c5ac570420f64a6ced6eea8da8fe255
Tea Hyeung, Son <53033449+thsvkd@users.noreply.github.com>
2020-08-05 09:17:40 +0900

    Update search_test.py

--- search_test.py
+++ search_test.py
@@ -9,7 +9,7 @@ from oauth2client.tools import argparser
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyBk2v0fQ8Uqwu04jD4aqa-bFFX7ucGS8Gs"
+DEVELOPER_KEY = ""
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
