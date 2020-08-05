6ef15314fe2952cee14c57ed03514eabb88e4b46
thsvkd <thsxogud1123@gmail.com>
2020-08-05 23:39:41 +0900

    cs

--- api_test.py
+++ api_test.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 
+import httplib2
+import os
+import sys
+
 from apiclient.discovery import build
 from apiclient.errors import HttpError
-from oauth2client.tools import argparser
-
+from oauth2client.tools import argparser, run_flow
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
 
 # Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
 # tab of
