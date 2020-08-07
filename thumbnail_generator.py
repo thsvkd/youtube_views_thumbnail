e9d1afa3f861ba1aca844f2ae0dc2c38beba8562
thsvkd <thsxogud1123@gmail.com>
2020-08-08 00:59:38 +0900

    Oauth success

--- thumbnail_generator.py
+++ thumbnail_generator.py
@@ -3,6 +3,7 @@ import numpy as np
 import pyperclip
 import matplotlib.pyplot as plt
 from PIL import ImageFont, ImageDraw, Image
+import getpass
 
 
 def make_colorcode(code):
@@ -20,15 +21,15 @@ def make_colorcode(code):
     return [r, g, b]
 
 
-def make_thumbnail_image(text, font_size, color):
+def make_thumbnail_image(text, font_size, font_color):
 
     ratio = 0.1
     winname = "BG"
-    r, g, b = make_colorcode(color)
+    r, g, b = make_colorcode(font_color)
     a = 0
 
     BG_img = cv2.imread(
-        "C:/Users/thsxo/Desktop/IMG_2199.JPG")
+        "C:/Users/%s/Desktop/IMG_2199.JPG" % getpass.getuser())
     BG_w, BG_h, BG_c = BG_img.shape
     BG_shape = [BG_w, BG_h, BG_c]
     BG_shape[0] *= ratio
@@ -57,43 +58,3 @@ def make_thumbnail_image(text, font_size, color):
 if __name__ == "__main__":
 
     make_thumbnail_image("이 영상의 조회수는 123입니다", 44, "b52b65")
-
-
-# import matplotlib.pyplot as plt
-# import numpy as np
-# from PIL import ImageFont, ImageDraw, Image
-
-# image1 = np.zeros((480, 640, 3), np.uint8)
-# image2 = Image.fromarray(image1)
-# draw = ImageDraw.Draw(image2)
-# draw.text((10, 20), "안녕하세요", font=ImageFont.truetype("./batang.ttc", 48), fill=(255,255,255))
-
-# plt.imshow(image2)
-# plt.show()
-
-##########################
-
-# import numpy as np
-# from PIL import ImageFont, ImageDraw, Image
-# import cv2
-# import time
-
-# ## Make canvas and set the color
-# img = np.zeros((200,400,3),np.uint8)
-# b,g,r,a = 0,255,0,0
-
-# ## Use cv2.FONT_HERSHEY_XXX to write English.
-# text = time.strftime("%Y/%m/%d %H:%M:%S %Z", time.localtime())
-# cv2.putText(img,  text, (50,50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (b,g,r), 1, cv2.LINE_AA)
-
-# ## Use simsum.ttc to write Chinese.
-# fontpath = "./simsun.ttc"
-# font = ImageFont.truetype(fontpath, 32)
-# img_pil = Image.fromarray(img)
-# draw = ImageDraw.Draw(img_pil)
-# draw.text((50, 100),  "国庆节/中秋节 快乐!", font = font, fill = (b, g, r, a))
-# img = np.array(img_pil)
-
-# ## Display
-# cv2.imshow("res", img);cv2.waitKey();cv2.destroyAllWindows()
-# cv2.imwrite("res.png", img)
